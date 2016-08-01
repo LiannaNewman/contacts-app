1df4acf3555bbc218e28768c299163e09ba57d56
Trevor Jones <trevordavidjones@gmail.com>
2016-08-01 09:00:34 -0400

    Added User and authentication

--- Gemfile
+++ Gemfile
@@ -26,8 +26,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'geocoder'
 gem 'seed_dump'
+gem 'quiet_assets'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,17 @@
 class ContactsController < ApplicationController
+
   def index
     @contacts = Contact.all
   end
 
   def show
-    @contact = Contact.find_by(id: params[:id])
+    contact = Contact.find_by(id: params[:id])
+    if current_user.contacts.include?(contact)
+      @contact = contact
+    else
+      flash[:warning] = "Hey sucka! Only look up your own contacts"
+      redirect_to '/contacts'
+    end
   end
 
   def new
@@ -15,7 +22,8 @@ class ContactsController < ApplicationController
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
-      bio: params[:bio]
+      bio: params[:bio],
+      user_id: current_user.id
     )
     flash[:success] = "Contact created."
     redirect_to "/contacts/#{@contact.id}"
@@ -29,7 +37,7 @@ class ContactsController < ApplicationController
     @contact = Contact.find_by(id: params[:id])
     @contact.update(
       first_name: params[:first_name],
-      last_name: params[:last_name], 
+      last_name: params[:last_name],
       email: params[:email],
       bio: params[:bio]
     )
